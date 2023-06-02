e7ed3c936a4e2a659d26c5633e7353b26e59822f
Michael <Michael@MICHAEL>
2023-06-01 22:29:53 -0500

    Agregar archivos de proyecto.

---
+++ Cliente2/ChatCLiente.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Client;
+
+namespace Cliente2
+{
+    internal class ChatCLiente
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Chat cliente #2");
+
+            const string serverAddress = "ws://127.0.0.1:8080/ws";
+            const string realmName = "realm1";
+            const string topicName = "com.myapp.chat";
+
+            DefaultWampChannelFactory channelFactory = new DefaultWampChannelFactory();
+            IWampChannel channel = channelFactory.CreateJsonChannel(serverAddress, realmName);
+
+            await channel.Open().ConfigureAwait(false);
+
+            Console.WriteLine("Conectando al servidor...");
+
+            IWampRealmProxy realm = channel.RealmProxy;
+            ISubject<string> subject = realm.Services.GetSubject<string>(topicName);
+
+            Console.WriteLine("Ingrese un usuario");
+            string username = Console.ReadLine();
+
+            subject.OnNext($"Ha iniciado sesion {username}");
+
+            IDisposable subscription = subject.Subscribe(message =>
+            {
+                Console.WriteLine(message);
+            });
+
+            Console.WriteLine($"{username} escriba un mensaje o 'exit' para salir del chat.");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+                if (input != "exit")
+                {
+                    string message = $"{username}: {input}";
+                    subject.OnNext(message);
+                }
+
+            } while (input != "exit");
+
+            subject.OnNext($"Se ha desconetado el usuario: {username}");
+
+            subscription.Dispose();
+            channel.Close();
+
+            Console.WriteLine("Desconexion del chat");
+        }
+    }
+}
---
+++ ServerWampSharp3/ChatServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Subjects;
+using System.Text;
+using System.Threading.Tasks;
+using WampSharp.V2;
+using WampSharp.V2.Realm;
+
+namespace ServerWampSharp3
+{
+    internal class ChatServer
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando servidor espere un momento....");
+            const string serverAddress = "ws://127.0.0.1:8080/ws";
+            const string realmName = "realm1";
+            const string topicName = "com.myapp.chat";
+
+            DefaultWampHost host = new DefaultWampHost(serverAddress);
+            host.Open();
+
+            Console.WriteLine("Server started.");
+
+            IWampHostedRealm realm = host.RealmContainer.GetRealmByName(realmName);
+            ISubject<string> subject = realm.Services.GetSubject<string>(topicName);
+
+            IDisposable subscription = subject.Subscribe(message =>
+            {
+                Console.WriteLine(message);
+            });
+
+            Console.WriteLine("Servidor iniciado en la ubicacion: "+serverAddress+" presione enter para terminar");
+
+            Console.ReadLine();
+
+            subscription.Dispose();
+            host.Dispose();
+
+            Console.WriteLine("Servidor detenido");
+        }
+    }
+}
